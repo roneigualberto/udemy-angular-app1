8811480c729fb379a4ec6244154c57845e080170
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-01 10:03:48 -0400

    Adicionando estilos ao componente topo com styles e styleUrls

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -3,7 +3,13 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-topo',
-    templateUrl: './topo.component.html'
+    templateUrl: './topo.component.html',
+   /* styles: [`.exemplo {
+        color: red
+        }`
+    ],*/
+    styleUrls: ['./topo.component.css']
+
    //template: '<p> Esse é o componente Topo </p>'
 })
 export class TopoComponent {
