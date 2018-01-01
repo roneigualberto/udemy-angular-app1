398f8f36d6c223fc443c7940be7f848baa992afc
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-01 09:40:39 -0400

    Utilizando o atributo de configuração template de @Component

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -4,6 +4,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'app-topo',
     templateUrl: './topo.component.html'
+   //template: '<p> Esse é o componente Topo </p>'
 })
 export class TopoComponent {
 
