419f9db940a6f980ef0f772d069fd6c97af79582
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-01 10:09:44 -0400

    Instanciando o componente com o seletor de atributo

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 
 @Component({
-    selector: 'app-topo',
+    selector: '[app-topo]',
     templateUrl: './topo.component.html',
    /* styles: [`.exemplo {
         color: red
