034aa7aaa4c988fc34d3bb34152766b300e3be56
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-01-01 17:30:23 -0400

    Criando a lógica de validação da respota do usuário

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -30,7 +30,17 @@ export class PainelComponent implements OnInit {
   }
 
   public verificarResposta(): void {
-    console.log('Verificar resposta: ', this.resposta);
+    
+    if (this.rodadaFrase.frasePtBr ==  this.resposta) {
+      alert('A tradução está correta');
+      this.rodada++;
+      this.rodadaFrase = this.frases[this.rodada];
+    }
+    else {
+      alert('A tradução está incorreta');
+    }
   }
+
+ 
 
 }
